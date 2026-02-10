31e883e3a098ba6ae54811a096a094f6283b63e6
hzy <28915578+hzy@users.noreply.github.com>
2026-02-10 18:18:56 +0800

    chore: enable eslint

--- packages/tools/perfetto-trace-processor/scripts/prepare.js
+++ packages/tools/perfetto-trace-processor/scripts/prepare.js
@@ -1,3 +1,6 @@
+// Copyright 2026 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
 import { mkdir, rm, readFile, copyFile, writeFile } from "node:fs/promises";
 import { extract } from "tar";
 import { pipeline } from "node:stream/promises";
